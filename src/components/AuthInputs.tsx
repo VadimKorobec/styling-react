6c2842d0148ff37c275ff56285f4e3db78d8c42c
Vadim <_s_e_t__@ukr.net>
2024-11-03 15:28:19 +0200

    add styled-components

--- src/components/AuthInputs.tsx
+++ src/components/AuthInputs.tsx
@@ -1,11 +1,45 @@
 import { useState } from "react";
 
+import { styled } from "styled-components";
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+interface StyledProps {
+  $invalid?: boolean;
+}
+
+const Label = styled.label<StyledProps>`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${(props) => (props.$invalid ? "#f87171" : "#6b7280")};
+`;
+
+const Input = styled.input<StyledProps>`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: ${(props) => (props.$invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${(props) => (props.$invalid ? "#ef4444" : "#374151")};
+  border: 1px solid ${(props) => (props.$invalid ? "#f73f3f" : "transparent")};
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
 const AuthInputs = () => {
   const [enteredEmail, setEnteredEmail] = useState<string>("");
   const [enteredPassword, setEnteredPassword] = useState<string>("");
   const [submitted, setSubmitted] = useState<boolean>(false);
 
-  function handleInputChange(identifier:string, value:string) {
+  function handleInputChange(identifier: string, value: string) {
     if (identifier === "email") {
       setEnteredEmail(value);
     } else {
@@ -22,26 +56,26 @@ const AuthInputs = () => {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p>
-          <label>Email</label>
-          <input
+          <Label $invalid={emailNotValid}>Email</Label>
+          <Input
+            $invalid={emailNotValid}
             type="email"
-            className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
-          <input
+          <Label $invalid={passwordNotValid}>Password</Label>
+          <Input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
